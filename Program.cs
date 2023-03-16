d5dcc03388fb6c17f83df7a32edf858ff8e861f2
Polina Kolesnikova <polina@example.com>
2023-03-16 23:23:47 +0400

    adding 52 exercise

--- Program.cs
+++ Program.cs
@@ -76,4 +76,62 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+int[,] ReturnMatrix(int rows, int columns, int min, int max)
+{
+    var rand = new Random();
+    int[,] myArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            myArray[i, j] = rand.Next(min, max + 1);
 
+        }
+
+    }
+    return myArray;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Please, enter quantity of rows and columns with space between: ");
+string[] line1 = Console.ReadLine()!.Split(" ");
+int rows = int.Parse(line1[0]);
+int columns = int.Parse(line1[1]);
+
+Console.Write("Please, enter min and max with space between: ");
+string[] line2 = Console.ReadLine()!.Split(" ");
+int min = int.Parse(line2[0]);
+int max = int.Parse(line2[1]);
+
+int[,] needMatrix = ReturnMatrix(rows, columns, min, max);
+PrintMatrix(needMatrix);
+
+double[] AverageColumns(int[,] myMatrix)
+{
+    double[] myArray = new double[myMatrix.GetLength(1)];
+    for (int j = 0;j < myMatrix.GetLength(1);j++)
+    {
+        double sum = 0;
+        for(int i = 0; i < myMatrix.GetLength(0); i++)
+        {
+            sum += myMatrix[i, j];
+        }
+        myArray[j] = Math.Round((sum / myMatrix.GetLength(0)), 2);
+    }
+    return myArray;
+}
+double[] needArray = AverageColumns(needMatrix);
+Console.WriteLine();
+Console.Write("Your answer is : ");
+Console.WriteLine(string.Join(" ", needArray));
